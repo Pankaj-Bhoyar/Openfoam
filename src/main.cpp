a5d91f2774f4e08a3d5893688330b1d01099bbfe
PANKAJ <PANKAJ@LAPTOP-BT3V3RH4>
2025-09-13 13:12:09 +0530

    START: 1st cpp configured project commit is pushed

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 13 Sept 2025
+ *      Author: PANKAJ
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+
